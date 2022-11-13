18d681ad0d1dfb244b3709b942198e45783b3e8d
yunojang <hanganda23@naver.com>
2022-11-13 22:11:44 +0900

    fix: clean

--- src/lib/client/index.ts
+++ src/lib/client/index.ts
@@ -2,48 +2,6 @@ import Axios, { AxiosRequestConfig } from "axios";
 import { API_URL, env } from "@/config";
 import { getToken } from "@/utils/storage";
 
-// interface CustomResponse<T = any> {
-//     data: T;
-//     refreshedToken?: string;
-// }
-
-// interface CustomInterceptorManager<T> extends AxiosInterceptorManager<T> {
-//     use<D>(
-//         onFulfilled?: (value: T) => D | Promise<D>,
-//         onRejected?: (error: any) => any,
-//         options?: AxiosInterceptorOptions
-//     ): number;
-// }
-
-// interface ClientInterface extends AxiosInstance {
-//     interceptors: {
-//         request: AxiosInterceptorManager<AxiosRequestConfig>;
-//         response: CustomInterceptorManager<AxiosResponse>;
-//     };
-//     // getUri(config?: AxiosRequestConfig): string;
-//     // request<T = any, R = AxiosResponse<T>, D = any>(
-//     //     config: AxiosRequestConfig<D>
-//     // ): Promise<R>;
-//     // get<T = any, R = AxiosResponse<T>, D = any>(
-//     //     url: string,
-//     //     config?: AxiosRequestConfig<D>
-//     // ): Promise<R>;
-//     // delete<T = any, R = AxiosResponse<T>, D = any>(
-//     //     url: string,
-//     //     config?: AxiosRequestConfig<D>
-//     // ): Promise<R>;
-//     // post<T = any, R = AxiosResponse<T>, D = any>(
-//     //     url: string,
-//     //     data?: D,
-//     //     config?: AxiosRequestConfig<D>
-//     // ): Promise<R>;
-//     // put<T = any, R = AxiosResponse<T>, D = any>(
-//     //     url: string,
-//     //     data?: D,
-//     //     config?: AxiosRequestConfig<D>
-//     // ): Promise<R>;
-// }
-
 const client = Axios.create({
     baseURL: API_URL,
 });
@@ -65,6 +23,7 @@ client.interceptors.request.use(authRequestInterceptor);
 // type issue
 client.interceptors.response.use(
     (response) => response.data,
+    // server response error
     (error) => {
         const message = error.response?.data?.message ?? error.message;
 
