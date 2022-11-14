85b1ce565cbcfc5dc3479411ebacc5b3137f80a3
yunojang <hanganda23@naver.com>
2022-11-14 23:34:48 +0900

    fe: login register mutation

--- src/features/auth/api/index.ts
+++ src/features/auth/api/index.ts
@@ -1,22 +1,12 @@
-import { useQuery } from "react-query";
+import { ReactNode, useCallback } from "react";
+import { useMutation, useQuery } from "react-query";
 import { clearToken, getToken, setToken } from "@/utils/storage";
-import { useToast } from "@chakra-ui/react";
 
 import { requestRegister, RegisterCredentialsDTO } from "./register";
 import { LoginCredentialsDTO, loginWithEmailAndPassword } from "./login";
 import { getUser } from "@/features/users/api/getUser";
-import { ReactNode } from "react";
 
-const register = async (data: RegisterCredentialsDTO) => {
-    const response = await requestRegister(data);
-    return response.user;
-};
-
-const login = async (data: LoginCredentialsDTO) => {
-    const response = await loginWithEmailAndPassword(data);
-    setToken(response.token);
-    return response.user;
-};
+import { useToast } from "@chakra-ui/react";
 
 const loadUser = async () => {
     const token = getToken();
@@ -26,6 +16,7 @@ const loadUser = async () => {
     }
 
     const response = await getUser();
+    if (!response) return;
     return response.user;
 };
 
@@ -34,21 +25,66 @@ const logout = () => {
     window.location.assign(window.location.origin);
 };
 
+const register = async (data: RegisterCredentialsDTO) => {
+    const response = await requestRegister(data);
+    return response.user;
+};
+
+const login = async (data: LoginCredentialsDTO) => {
+    const response = await loginWithEmailAndPassword(data);
+
+    setToken(response.token);
+    return response.user;
+};
+
 export const useAuth = () => {
     const toast = useToast();
+
     const { data: user } = useQuery(["my"], loadUser, {
+        suspense: false,
+        useErrorBoundary: false,
         onError: (err) => {
             toast({
+                title: "Error",
+                description: err as ReactNode,
                 status: "error",
                 position: "top-right",
-                description: err as ReactNode,
+                duration: 1500,
+                isClosable: true,
+            });
+        },
+    });
+
+    const { mutateAsync: loginFn } = useMutation({
+        mutationFn: login,
+        onError: (err) => {
+            toast({
+                title: err as string,
+                status: "error",
+                position: "top-right",
+                duration: 1500,
+                isClosable: true,
+            });
+        },
+    });
+
+    const { mutateAsync: resgiterFn } = useMutation({
+        mutationFn: register,
+        onSuccess: () => {
+            toast({
+                title: "Success",
+                description: "Create new user",
+                status: "success",
+                position: "top-right",
+                duration: 1500,
+                isClosable: true,
             });
         },
     });
 
     return {
-        register,
-        login,
+        register: resgiterFn,
+        login: loginFn,
         user,
         logout,
     };
